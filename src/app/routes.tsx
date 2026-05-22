872fc84b04ff24f8ed719221916ddad6ef2025f2
Sukh Singh <sukh@somebodytotalkto.com>
2026-05-23 00:12:50 +0530

    Redirect COH legacy route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react';
 import { createHashRouter } from 'react-router';
 import { SpotlightLayout } from './components/SpotlightLayout';
 import { SpotlightPageCOH } from './pages/SpotlightPageCOH';
 
+const CITY_OF_HOPE_URL = 'https://cityofhope.oneamyloidosisvoice.com/';
+
+function CityOfHopeRedirect() {
+  useEffect(() => {
+    window.location.replace(CITY_OF_HOPE_URL);
+  }, []);
+
+  return null;
+}
+
 export const router = createHashRouter([
+  {
+    path: '/spotlight/coh/v1',
+    Component: CityOfHopeRedirect,
+  },
   // City of Hope custom domain root.
   {
     path: '/',
